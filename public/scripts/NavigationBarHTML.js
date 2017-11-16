a4bc071d62efbc611b88c8148413ea44ebe1f80f
Mohamed Wagih <mohamedwagih96@outlook.com>
2017-11-16 22:45:06 +0200

    Added : Student page

---
+++ public/scripts/NavigationBarHTML.js
@@ -0,0 +1,14 @@
+document.write(
+'<div class="leftCol" id="f">'+
+  '<div class="NvBar">'+
+    '<div class="ppContainer">'+
+      '<img class="profilePic" src="images/pp.png" alt="User PP"/>'+
+    '</div>'+
+    '<div class="NvBtns">'+
+      '<button class="NvBtn" href=""><span class="pTxt">My Profile &nbsp;</span><span class="glyphicon glyphicon-user"></span></button>'+
+      '<button class="NvBtn" href=""><span class="ttTxt">Timetable &nbsp;</span><span class="glyphicon glyphicon-calendar"></span></button>'+
+      '<button class="NvBtn" href=""><span class="loTxt">Log Out &nbsp;</span><span class="glyphicon glyphicon-log-out"></span></button>'+
+    '</div>'+
+  '</div>'+
+'</div>'
+);
